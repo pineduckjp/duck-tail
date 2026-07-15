b235a47ff18741488ec8fbeb1f91f51aa57c29c9
pineduckjp <hello@pineduck.jp>
2026-07-15 10:15:42 +0900

    refactor: ヘッダーとフッターのリンク参照仕様を変更
    - グローバル定数で外部サイトのリンクを管理
    - 同オリジンのリンクはルート基準のパスにする(例: /page1/)

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = "アヒルのしっぽ";
 export const SITE_DESCRIPTION = "不定期で更新しているPineDuckのブログです。";
+
+export const PINEDUCK_URL = "https://pineduck.jp";
+export const LP_URL = "https://pineduck.jp/?scene=duck-tail";
